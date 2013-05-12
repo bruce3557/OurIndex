d81b7ae6967df62b30ee65b72a5279e946f0de84
Bruce Kuo <bruce3557@gmail.com>
2013-05-12 22:28:52 +0800

    Remove unused library
    Remove sdsl and use sdsl-lite.

--- lib/sdsl/include/sdsl/config.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef SDSL_CONFIG
-#define SDSL_CONFIG
-
-#define CMAKE_SOURCE_DIR /home/bruce3557/SuffixTreeBased/OurIndex/lib/sdsl/sdsl
-
-#endif
--- lib/sdsl/include/sdsl/vectors.hpp
+++
@@ -1,6 +0,0 @@
-/** \defgroup int_vector int_vector */
-
-#include "int_vector.hpp"
-#include "enc_vector.hpp"
-#include "enc_vector_theo.hpp"
-#include "enc_vector_dna.hpp"
