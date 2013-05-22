b3ac697e92b2b6104001ba463d20ff5a3c0ba0ad
Bruce Kuo <bruce3557@gmail.com>
2013-05-22 20:05:21 +0800

    Add node id to metadata

--- query/query_processing.cpp
+++ query/query_processing.cpp
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <set>
+
 #include <sdsl/suffix_trees.hpp>
 #include <sdsl/util.hpp>
 
@@ -17,10 +19,21 @@ using sdsl;
 
 typedef cst_sada<> tCST;
 
+struct meta_node {
+  int id, pos;
+
+  meta_node(){}
+  meta_node(int _id, int _pos): id(_id), pos(_pos){}
+
+  bool operator < (const meta_node &t) const {
+    return id < t.id;
+  }
+} ;
+
 bool load_metadata(vector<int> &metadata, char *filename) {
   FILE *fp = fopen(filename, "r");
-  int x;
-  while( fscanf(fp, "%d", &x) != EOF )
+  int x, y;
+  while( fscanf(fp, "%d%d", &x, &y) != EOF )
     metadata.push_back(x);
   fclose(fp);
 }
@@ -41,6 +54,37 @@ int main(int argc, char *argv[]) {
   if( !load_metadata(metadata, "metadata.idx") ) {
     fprintf(stderr, "Loading metadata error...");
     return 0;
+  }
+
+  char s_query[510];
+  while(1) {
+    fprintf(stdout, "Please enter the query string:\n");
+    if( !gets(s_query) )  break;
+    //
+    // TODO (Bruce Kuo):
+    //  Add translate to integer functions
+    //
+    int length = strlen(s_query);
+
+    //
+    // TODO (Bruce Kuo):
+    //  (1) compare with forward search and backword search
+    //      now we use forward search
+    //  (2) now we assume that the number of query words is less than 8
+    //
+    node_t v = cst.root();
+    string query = string(s_query);
+    string::iterator it = query.begin();
+    for(uint64_t pos=0;it != query.end();++it) {
+      if(forward_search(cst, v, it-query.begin(), *it, pos) > 0) {
+        
+      } else {
+        break;
+      }
+    }
+    int node_id = v.id();
+
+
   }
 
   return 0;
